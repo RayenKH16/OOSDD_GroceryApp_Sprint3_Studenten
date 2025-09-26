08af23d82e5cc49d9fd544c8ddc164f11516e999
Rayen87 <119052841+Rayen87@users.noreply.github.com>
2025-09-26 20:39:51 +0200

    Update TestHelpers.cs
    test fix

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -1,5 +1,4 @@
 using Grocery.Core.Helpers;
-
 namespace TestCore
 {
     public class TestHelpers
@@ -9,8 +8,7 @@ namespace TestCore
         {
         }
 
-
-        //Happy flow
+        // Correcte wachtwoorden
         [Test]
         public void TestPasswordHelperReturnsTrue()
         {
@@ -26,19 +24,69 @@ namespace TestCore
             Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
         }
 
-
-        //Unhappy flow
+        //Unhappy flow - Verkeerde wachtwoorden
         [Test]
         public void TestPasswordHelperReturnsFalse()
         {
-            Assert.Pass(); //Zelf uitwerken
+            // Test met verkeerd wachtwoord
+            string correctPassword = "user3";
+            string wrongPassword = "wrongpassword";
+            string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
+
+            // Het verkeerde wachtwoord zou false moeten returnen
+            Assert.IsFalse(PasswordHelper.VerifyPassword(wrongPassword, passwordHash));
         }
 
+        // Deze TestCases zijn EXPRES fout gemaakt (verkeerde hashes zonder '=' aan het eind)
+        // Om te testen dat PasswordHelper correct omgaat met ongeldige hashes
         [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
         [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
         public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
         {
-            Assert.Fail(); //Zelf uitwerken zodat de test slaagt!
+            // Deze test verwacht dat de verificatie faalt vanwege ongeldige hash formatting
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
+        }
+
+        // Extra edge case tests voor betere test coverage
+        [Test]
+        public void TestPasswordHelper_NullPassword_ReturnsFalse()
+        {
+            string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
+            Assert.IsFalse(PasswordHelper.VerifyPassword(null, passwordHash));
+        }
+
+        [Test]
+        public void TestPasswordHelper_EmptyPassword_ReturnsFalse()
+        {
+            string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
+            Assert.IsFalse(PasswordHelper.VerifyPassword("", passwordHash));
+        }
+
+        [Test]
+        public void TestPasswordHelper_NullHash_ReturnsFalse()
+        {
+            Assert.IsFalse(PasswordHelper.VerifyPassword("user3", null));
+        }
+
+        [Test]
+        public void TestPasswordHelper_EmptyHash_ReturnsFalse()
+        {
+            Assert.IsFalse(PasswordHelper.VerifyPassword("user3", ""));
+        }
+
+        [Test]
+        public void TestPasswordHelper_InvalidHashFormat_ReturnsFalse()
+        {
+            // Hash zonder punt (.) separator
+            Assert.IsFalse(PasswordHelper.VerifyPassword("user3", "invalidhashwithoutdotseparator"));
+        }
+
+        [TestCase("user1", "wronghash")]
+        [TestCase("user2", "anotherwronghash")]
+        [TestCase("user3", "completelywrongformat")]
+        public void TestPasswordHelper_WrongPasswords_ReturnsFalse(string password, string wrongHash)
+        {
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, wrongHash));
         }
     }
 }
